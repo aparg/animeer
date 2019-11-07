97994c4f5266b2a008cca4d93c8db76ff2dbc95b
Robin Panta <hacktivistic@gmail.com>
2019-11-07 18:21:18 +0530

    feat(*): add new routes and randomizor

--- controllers/home.js
+++ controllers/home.js
@@ -11,7 +11,14 @@ const router = express.Router()
 var animes = require('../models/animes')
 
 router.get('/', (req, res) => {
-    res.render('pages/home', { animes: animes, title: "Home Page" })
+
+    // For Random Anime
+
+    var array = Object.values(animes) // To get random element, first getting the array of values of animes i.e. converting to array
+    var random_anime = array[Math.floor(Math.random()*array.length)] // then selecting an random index using random number gererated from Math.random()
+    random_anime.index = Object.keys(animes)[array.indexOf(random_anime)] // additionally, adding the index of the random anime for url generation purpose,, both random anime values and key shall have have index
+
+    res.render('pages/home', { anime: random_anime, title: "Home Page" })
 })
 
 module.exports = router
--- models/animes.js
+++ models/animes.js
@@ -4,26 +4,32 @@ module.exports = {
         name: 'One Piece',
         hero: 'Monkey D. Luffy',
         writer: 'Eiichiro Oda',
-        synopsis : "Get aboard the Straw Hats' ship and experience the journey of a pirate king!"
+        synopsis : "Get aboard the Straw Hats' ship and experience the journey of a pirate king!",
+        poster: '/images/one-piece.jfif'
     },
 
     'dr-stone': {
         name: 'Dr. Stone',
         hero: 'Ishigami Senku',
         writer: 'Boichi',
-        synopsis : 'The mordern world has collapsed and a doctor along with his crew use science to make a new era of mordern science   from scratch.'
+        synopsis : 'The mordern world has collapsed and a doctor along with his crew use science to make a new era of mordern science   from scratch.',
+        poster: '/images/dr-stone.jfif'
     },
 
     'naruto' : {
         name: 'Naruto',
         hero: 'Naruto Uzumaki',
         writer: 'Masashi Kisimoto',
-        synopsis : "From the hated kid of the village to the protector of people, will Naruto's dream of becoming a hokage change into  reality?"
+        synopsis : "From the hated kid of the village to the protector of people, will Naruto's dream of becoming a hokage change into  reality?",
+        poster: '/images/naruto.jfif'
     },
 
     'bleach' : {
         name :'Bleach',
-        hero: 'Ichigo'
+        hero: 'Ichigo',
+        writer: 'Kubo Tite',
+        synopsis: "Orewa shinigami ni naru!",
+        poster: '/images/bleach.jfif'
     }
 
 
